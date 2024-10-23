180eda727c13731cdb97dff256543bff8dbe3a1b
Afrin Nahar <afrinnahar1999@gmail.com>
2024-10-23 23:40:28 +0600

    modified input style in category form

--- client/src/components/Form/CategoryForm.tsx
+++ client/src/components/Form/CategoryForm.tsx
@@ -4,19 +4,20 @@ import { Input } from "@/components/ui/input";
 const CategoryForm = ({ handleSubmit, value, setValue }) => {
   return (
     <form onSubmit={handleSubmit} className="space-y-4">
-      <div className="mb-3">
+      <div className="mb-3 flex gap-4 items-center justify-between">
         <Input
           type="text"
           placeholder="Enter new category"
           value={value}
           onChange={(e) => setValue(e.target.value)}
-          className="w-full"
+          className="w-full flex-3"
         />
+        <Button type="submit" className="w-full flex-1">
+          Create
+        </Button>
       </div>
 
-      <Button type="submit" className="w-full">
-        Submit
-      </Button>
+
     </form>
   );
 };
