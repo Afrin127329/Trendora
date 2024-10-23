e5d134d7cc54423cbff3f453cd0f1a1b90072803
Afrin Nahar <afrinnahar1999@gmail.com>
2024-10-23 23:34:35 +0600

    added skeleton in category creation in admin panel

--- client/src/Routes/AdminRoute.tsx
+++ client/src/Routes/AdminRoute.tsx
@@ -1,6 +1,8 @@
+import Layout from "@/components/Layout";
+import { Skeleton } from "@/components/ui/skeleton";
 import { Loader } from "lucide-react";
 import { useEffect, useState } from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import { useAuth } from "../context/auth";
 import axiosInstance from "../lib/axiosInstance";
 
@@ -10,13 +12,17 @@ export default function AdminRoute() {
   // @ts-ignore
   const [auth, setAuth] = useAuth();
 
+  const location = useLocation();
+  const { pathname } = location;
+  console.log(pathname)
+
   useEffect(() => {
     const authCheck = async () => {
       try {
         const token = auth?.token;
         const res = await axiosInstance.get("/api/v1/auth/admin-auth", {
           headers: {
-            Authorization: `Bearer ${token}`,  // Pass token in headers
+            Authorization: `Bearer ${token}`,
           },
         });
         if (res.data.ok) {
@@ -32,5 +38,22 @@ export default function AdminRoute() {
     if (auth?.token) authCheck();
   }, [auth?.token]);
 
-  return ok ? <Outlet /> : <Loader path="" />;
+  return ok ? <Outlet /> : (
+    <Layout title="Admin Login - Trendora">
+      {
+        pathname === "/dashboard/admin/create-category" ? (
+          <div className="flex flex-col space-y-3 w-full min-h-full justify-center items-center mt-4">
+            <Skeleton className="h-[225px] w-[350px] rounded-xl" />
+            <div className="space-y-2">
+              <Skeleton className="h-4 w-[350px]" />
+              <Skeleton className="h-4 w-[300px]" />
+            </div>
+          </div>
+        ) : (
+          <Skeleton className="h-4 w-[250px]" />
+        )
+      }
+      <Loader path="" />
+    </Layout>
+  );
 }
